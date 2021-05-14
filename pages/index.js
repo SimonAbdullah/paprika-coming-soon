495c2f24c9f07a378ed5640edaeec6bd851a540d
Ali <aligeblawe@gmail.com>
2021-05-15 00:21:10 +0300

    re-design comming soon page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/comingSoon.module.css'
+import { faPhone,faInbox } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Home() {
   return (
@@ -8,23 +10,27 @@ export default function Home() {
         <title>Paprika | Coming Soon</title>
         <link rel="icon" href="/png.png" />
       </Head>
-  
-      <section className={styles.comingSoon}>
-      <img src="/png.png" className={styles.img}/>
-      <div className={styles.hero}>
-       
-        <h1 className={styles.text}>Coming Soon</h1>
-        <p className={styles.p}>We are launching soon, please stay tuned.</p>
-        <hr className={styles.hr} />
-        {/* <p className={styles.l}></p> */}
-      </div>
-      </section>
-      
+      <div className={styles.opening_soon_info}>
+            <div className={styles.wrapper}>
+                <div className={styles.header}>
+                    <div className={styles.logo}>
+                        <a className={styles.brand_logo} href="/"><img src="/png.png" className={styles.img}/></a>
+                    </div>
+                </div>
 
-     
-     
-
-
+                <div className={styles.coming_back}>
+                    <h1>We Are Launching Soon</h1>
+                    <p className={styles.t_text}>
+                      Get in touch with us
+                    </p>
+                    <div className={styles.info}>
+                    <span><FontAwesomeIcon icon={faPhone} inverse /> +46 0733501904</span>
+                    <span><FontAwesomeIcon icon={faInbox} />  support@paprika-sy.com</span>
+                    </div>
+                   
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
